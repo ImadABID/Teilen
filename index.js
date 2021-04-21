145f9c5280e7215b55e3885035e7e0a2958a6937
ABIED Imad <Imad.Abied@bordeaux-inp.fr>
2021-04-22 00:18:36 +0200

    Reading form

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express();
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('Data_Base.db');
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -20,8 +23,17 @@ app.get('/inscription', (req, res)=>{
 
 app.post('/inscription',(req, res)=>{
     // Checking info & saving data
+    db.serialize(function() {
+
+        db.run(`
+            INSERT INTO Users(pseudo, email, password)
+            VALUES
+                (?, ?, ?);
+        `, req.body.pseudo, req.body.email, req.body.password);
+    
+    });
     res.redirect('/');
 });
 
-
+db.close();
 app.listen(3000);
